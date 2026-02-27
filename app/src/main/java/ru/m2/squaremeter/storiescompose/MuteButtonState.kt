122b5241a9422d30febdf71033d73ea66a82519b
falzanvs <falzanvs@m2.ru>
2026-02-27 11:43:35 +0300

    main activity

---
+++ app/src/main/java/ru/m2/squaremeter/storiescompose/MuteButtonState.kt
@@ -0,0 +1,6 @@
+package ru.m2.squaremeter.storiescompose
+
+data class MuteButtonState(
+    val changed: Boolean = false,
+    val mute: Boolean = false
+)
--- app/src/main/java/ru/m2/squaremeter/storiescompose/StoriesPreviews.kt
+++ app/src/main/java/ru/m2/squaremeter/storiescompose/StoriesPreviews.kt
@@ -12,20 +12,5 @@ val STORIES_PREVIEW_LIST = listOf(
         id = "id1",
         imageData = R.drawable.ic_launcher_background,
         title = "1",
-    ),
-    UiStoriesPreviewData(
-        id = "id2",
-        imageData = R.drawable.ic_launcher_background,
-        title = "2",
-    ),
-    UiStoriesPreviewData(
-        id = "id3",
-        imageData = R.drawable.ic_launcher_background,
-        title = "3",
-    ),
-    UiStoriesPreviewData(
-        id = "id4",
-        imageData = R.drawable.ic_launcher_background,
-        title = "4",
     )
 )
