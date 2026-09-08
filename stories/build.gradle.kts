d7af732c7f2deb332cd39997b26600eb4f3748e9
falzanvs <falzanvs@m2.ru>
2026-09-08 13:44:35 +0300

    version++

--- stories/build.gradle.kts
+++ stories/build.gradle.kts
@@ -60,7 +60,7 @@ extensions.configure(PublishingExtension::class.java) {
         create("release", MavenPublication::class.java) {
             groupId = "ru.m2.squaremeter"
             artifactId = "stories"
-            version = "1.3.11"
+            version = "1.3.12"
             afterEvaluate {
                 from(components["release"])
             }
